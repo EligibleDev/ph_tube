2862e00914c33a3aada3c8b3382daa2ad5bf1aa7
EligibleDev <eligibledev@gmail.com>
2023-09-01 15:54:36 +0600

    prochur error

---
+++ js/app.js
@@ -0,0 +1,42 @@
+const loadCategory = async () => {
+    const response = await fetch('https://openapi.programming-hero.com/api/videos/categories');
+    const data = await response.json();
+
+    const tabContainer = document.getElementById('tab-container');
+    data.data.forEach(category => {
+        const div = document.createElement('div');
+        div.innerHTML = `<a onclick ="loadVideo('${category.category_id}')" class="tab accent-blackk grey-bg">${category.category}</a>`;
+        tabContainer.appendChild(div);
+    });
+}
+
+const loadVideo = async (categoryId) => {
+    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
+    const data = await response.json();
+
+
+    const cardContainer = document.getElementById('card-container')
+    data.data.forEach(video => {
+        const div = document.createElement('div');
+        div.innerHTML = `<div class="card cursor-pointer">
+                            <img class="w-full rounded-xl h-52" src="${video.thumbnail}" alt="">
+                            <div class="info">
+                                <img class="w-10 h-10 rounded-full" src="${video.authors[0].profile_picture}" alt="">
+                                <div class="video-info">
+                                    <h3 class="post-title">${video.title}</h3 class="post-title">
+                                    <div>
+                                        <p>${video.authors[0].profile_name}</p>
+                                        <img class="" id="bluetick" src="${video.authors[0]?.verified ? `../images/bluetick.png` : ""}" alt="">
+                                    </div>
+                                    <p>${video.others.views} views</p>
+                                </div>
+                            </div>
+                        </div>`
+                        ;
+        cardContainer.appendChild(div);
+        console.log(video)
+
+    })
+}
+
+loadCategory();
